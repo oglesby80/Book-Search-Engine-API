43f3a03cc6a3340438ad09ad7c3b1a20e918855f
oglesby80 <OglesbyRodriguez@gmail.com>
2024-11-16 12:37:30 -0500

    63rd push

--- server/src/server.ts
+++ server/src/server.ts
@@ -39,7 +39,13 @@ async function startApolloServer() {
 
   // Serve static assets in production
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    // Serve React static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // Serve index.html for non-API routes
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
   }
 
   // Apply routes
@@ -62,4 +68,5 @@ async function startApolloServer() {
 }
 
 startApolloServer();
+
 
